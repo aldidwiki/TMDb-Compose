ce0091192fde731fc886bca88371493a38ed4650
Aldi Prahasta <aldidwiki@gmail.com>
2024-04-25 08:50:48 +0700

    optimize some codes

---
+++ app/src/main/java/com/aldiprahasta/tmdb/ui/details/ContentDetailCard.kt
@@ -0,0 +1,266 @@
+package com.aldiprahasta.tmdb.ui.details
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.aldiprahasta.tmdb.domain.model.MovieDetailDomainModel
+import com.aldiprahasta.tmdb.ui.components.ImageLoader
+import com.aldiprahasta.tmdb.ui.components.ImageLoaderBackdrop
+import com.aldiprahasta.tmdb.ui.components.ImageType
+import com.aldiprahasta.tmdb.utils.formatVoteAverage
+
+@Composable
+fun ContentDetailCard(
+        movieDetailDomainModel: MovieDetailDomainModel,
+        colorPalette: Triple<Color, Color, Color>,
+        modifier: Modifier = Modifier
+) {
+    Surface(
+            color = colorPalette.first,
+            shadowElevation = 6.dp,
+            modifier = modifier.fillMaxWidth()
+    ) {
+        Column {
+            movieDetailDomainModel.backdropPath?.let { path ->
+                ImageLoaderBackdrop(
+                        imagePath = path,
+                        imageType = ImageType.BACKDROP
+                )
+            }
+            ContentDetailPosterWithInfo(
+                    posterPath = movieDetailDomainModel.posterPath,
+                    title = movieDetailDomainModel.title,
+                    releaseDate = movieDetailDomainModel.releaseDate,
+                    runtime = movieDetailDomainModel.runtime,
+                    tagline = movieDetailDomainModel.tagline,
+                    genres = movieDetailDomainModel.genres,
+                    certification = movieDetailDomainModel.certification,
+                    colorPalette = colorPalette,
+                    modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .padding(top = 10.dp)
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            ContentDetailUserScoreWithTrailer(
+                    voteAverage = movieDetailDomainModel.voteAverage,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    colorPalette = colorPalette
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            ContentOverview(
+                    overview = movieDetailDomainModel.overview,
+                    colorPalette = colorPalette,
+                    modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .padding(bottom = 16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ContentOverview(
+        overview: String,
+        colorPalette: Triple<Color, Color, Color>,
+        modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+                text = "Overview",
+                style = MaterialTheme.typography.titleMedium,
+                color = colorPalette.second
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+                text = overview,
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Justify,
+                color = colorPalette.third
+        )
+    }
+}
+
+@Composable
+private fun ContentDetailUserScoreWithTrailer(
+        voteAverage: Double,
+        colorPalette: Triple<Color, Color, Color>,
+        modifier: Modifier = Modifier
+) {
+    Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = modifier.fillMaxWidth()
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            CircularProgressIndicator(
+                    modifier = Modifier.size(40.dp),
+                    strokeWidth = 3.dp,
+                    trackColor = colorPalette.first,
+                    color = colorPalette.second,
+                    progress = {
+                        voteAverage.formatVoteAverage() / 100f
+                    }
+            )
+            Text(
+                    text = "${voteAverage.formatVoteAverage()}%",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = colorPalette.third
+            )
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(
+                text = "User Score",
+                style = MaterialTheme.typography.labelMedium,
+                color = colorPalette.third
+        )
+        Spacer(modifier = Modifier.size(20.dp))
+        Text(
+                text = "\u007C",
+                style = MaterialTheme.typography.labelMedium,
+                color = colorPalette.second
+        )
+        TextButton(onClick = { }) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                        imageVector = Icons.Default.PlayArrow,
+                        contentDescription = null,
+                        tint = colorPalette.second
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(
+                        text = "Play Trailer",
+                        color = colorPalette.third
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ContentDetailPosterWithInfo(
+        posterPath: String?,
+        title: String,
+        releaseDate: String,
+        runtime: String,
+        tagline: String,
+        genres: String,
+        certification: String,
+        colorPalette: Triple<Color, Color, Color>,
+        modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        ImageLoader(
+                imagePath = posterPath,
+                imageType = ImageType.POSTER,
+                modifier = Modifier
+                        .width(100.dp)
+                        .height(150.dp)
+        )
+        Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                        .weight(1f)
+                        .padding(top = 12.dp)
+                        .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = colorPalette.second
+            )
+            Text(
+                    text = releaseDate,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = colorPalette.third
+            )
+            Row {
+                Text(
+                        text = runtime,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = colorPalette.third
+                )
+                Text(
+                        text = "\u2022",
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                        color = colorPalette.third
+                )
+                Text(
+                        text = certification,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = colorPalette.third
+                )
+            }
+            Text(
+                    text = genres,
+                    style = MaterialTheme.typography.bodySmall,
+                    textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = colorPalette.third
+            )
+            Text(
+                    text = tagline,
+                    style = MaterialTheme.typography.bodySmall,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Light,
+                    textAlign = TextAlign.Center,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(top = 6.dp),
+                    color = colorPalette.third
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContentDetailCardPreview() {
+    ContentDetailCard(
+            movieDetailDomainModel = MovieDetailDomainModel(
+                    title = "Dune: Part Two",
+                    posterPath = null,
+                    releaseDate = "27 February 2024",
+                    runtime = "2h 27m",
+                    tagline = "Long live the fighters.",
+                    overview = "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.",
+                    id = 693134,
+                    voteAverage = 8.291,
+                    genres = "Adventures, Science Fiction",
+                    certification = "PG-13",
+                    backdropPath = null,
+                    casts = emptyList()
+            ),
+            colorPalette = Triple(Color.White, Color.Black, Color.Black)
+    )
+}
+
