0b779ede7d15b713bc19dedda206bddcd272d019
Aldi Prahasta <aldidwiki@gmail.com>
2024-04-29 13:45:32 +0700

    add error drawable on image loader

--- app/src/main/java/com/aldiprahasta/tmdb/ui/components/CommonUi.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/ui/components/CommonUi.kt
@@ -7,20 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.aldiprahasta.tmdb.R
 
 @Composable
 fun ImageLoader(
@@ -32,6 +30,7 @@ fun ImageLoader(
             model = ImageRequest.Builder(LocalContext.current)
                     .data("https://image.tmdb.org/t/p/${imageType.value}/$imagePath")
                     .placeholder(ColorDrawable(Color.GRAY))
+                    .error(R.drawable.ic_broken_image)
                     .crossfade(true)
                     .build(),
             contentDescription = null,
