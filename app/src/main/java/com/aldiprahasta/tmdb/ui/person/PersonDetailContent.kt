5757e7059b04aa4d22572260fab7b652519ad92d
Aldi Prahasta <aldidwiki@gmail.com>
2024-04-29 11:48:06 +0700

    make biography text expandable

--- app/src/main/java/com/aldiprahasta/tmdb/ui/person/PersonDetailContent.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/ui/person/PersonDetailContent.kt
@@ -1,5 +1,6 @@
 package com.aldiprahasta.tmdb.ui.person
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -15,23 +17,30 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -246,14 +255,38 @@ private fun PersonBiography(
         biography: String,
         modifier: Modifier = Modifier
 ) {
+    var isExpanded by remember { mutableStateOf(false) }
+
     Column(modifier = modifier.fillMaxWidth()) {
         Text(text = "Biography", style = MaterialTheme.typography.titleLarge)
         Spacer(modifier = Modifier.size(10.dp))
         Text(
                 text = biography,
                 style = MaterialTheme.typography.bodySmall,
-                textAlign = TextAlign.Justify
+                textAlign = TextAlign.Justify,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                        .animateContentSize()
+                        .heightIn(max = if (isExpanded) 500.dp else 70.dp)
         )
+
+        if (biography.length > 200) {
+            TextButton(
+                    onClick = {
+                        isExpanded = !isExpanded
+                    },
+                    modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                        imageVector = if (isExpanded)
+                            Icons.Default.KeyboardArrowUp
+                        else
+                            Icons.Default.KeyboardArrowDown,
+                        contentDescription = null
+                )
+                Text(text = if (isExpanded) "Collapse" else "Expand")
+            }
+        }
     }
 }
 
