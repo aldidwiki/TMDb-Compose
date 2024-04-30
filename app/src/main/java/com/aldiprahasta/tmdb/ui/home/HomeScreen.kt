f96c482ae77aa2f0759c4a105ca086ce2e372ca8
Aldi Prahasta <aldidwiki@gmail.com>
2024-04-30 10:30:13 +0700

    add tv screen

--- app/src/main/java/com/aldiprahasta/tmdb/ui/home/HomeScreen.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/ui/home/HomeScreen.kt
@@ -1,8 +1,14 @@
 package com.aldiprahasta.tmdb.ui.home
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -12,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
 import com.aldiprahasta.tmdb.ui.movie.MovieScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -42,4 +49,47 @@ fun HomeScreen(
                 modifier = modifier.padding(innerPadding)
         )
     }
+}
+
+@Composable
+fun TMDbBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(modifier = modifier) {
+        NavigationBarItem(
+                selected = true,
+                onClick = {
+
+                },
+                icon = {
+                    Icon(
+                            imageVector = Icons.Default.Face,
+                            contentDescription = null
+                    )
+                },
+                label = {
+                    Text(text = "Movie")
+                }
+        )
+
+        NavigationBarItem(
+                selected = false,
+                onClick = {
+
+                },
+                icon = {
+                    Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null
+                    )
+                },
+                label = {
+                    Text(text = "Tv Show")
+                }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TMDbBottomNavigationPreview() {
+    TMDbBottomNavigation()
 }
---
+++ app/src/main/java/com/aldiprahasta/tmdb/ui/tv/TvScreen.kt
@@ -0,0 +1,111 @@
+package com.aldiprahasta.tmdb.ui.tv
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.aldiprahasta.tmdb.domain.model.TvDomainModel
+import com.aldiprahasta.tmdb.ui.components.ContentItem
+import com.aldiprahasta.tmdb.ui.components.ErrorScreen
+import com.aldiprahasta.tmdb.ui.components.LoadingScreen
+import com.aldiprahasta.tmdb.utils.doIfError
+import com.aldiprahasta.tmdb.utils.doIfLoading
+import com.aldiprahasta.tmdb.utils.doIfSuccess
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun TvScreen(
+        onItemClicked: (tvId: Int) -> Unit,
+        modifier: Modifier = Modifier
+) {
+    val tvViewModel: TvViewModel = koinViewModel()
+    val tvData by tvViewModel.popularTv.collectAsStateWithLifecycle()
+
+    Box(modifier = modifier.fillMaxWidth()) {
+        tvData.doIfLoading {
+            LoadingScreen()
+        }
+
+        tvData.doIfError { throwable, _ ->
+            ErrorScreen(errorMessage = throwable.localizedMessage ?: "")
+        }
+
+        tvData.doIfSuccess { popularTvList ->
+            TvContent(
+                    popularTvList = popularTvList,
+                    onItemClicked = onItemClicked
+            )
+        }
+    }
+}
+
+@Composable
+fun TvContent(
+        popularTvList: List<TvDomainModel>,
+        onItemClicked: (tvId: Int) -> Unit,
+        modifier: Modifier = Modifier
+) {
+    LazyColumn(
+            modifier = modifier,
+            contentPadding = PaddingValues(10.dp)
+    ) {
+        itemsIndexed(popularTvList) { index, item ->
+            ContentItem(
+                    title = item.title,
+                    releaseDate = item.releaseDate,
+                    characterName = null,
+                    posterPath = item.posterPath,
+                    onItemClicked = {
+                        onItemClicked(item.tvId)
+                    }
+            )
+
+            if (index < popularTvList.lastIndex) {
+                HorizontalDivider(Modifier.padding(vertical = 16.dp))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TvContentPreview() {
+    TvContent(
+            popularTvList = listOf(
+                    TvDomainModel(
+                            posterPath = null,
+                            tvId = 9989,
+                            title = "omnesque",
+                            releaseDate = "dicam"
+                    ),
+                    TvDomainModel(
+                            posterPath = null,
+                            tvId = 9989,
+                            title = "omnesque",
+                            releaseDate = "dicam"
+                    ),
+                    TvDomainModel(
+                            posterPath = null,
+                            tvId = 9989,
+                            title = "omnesque",
+                            releaseDate = "dicam"
+                    ),
+                    TvDomainModel(
+                            posterPath = null,
+                            tvId = 9989,
+                            title = "omnesque",
+                            releaseDate = "dicam"
+                    )
+            ),
+            onItemClicked = {}
+    )
+}
