7cb1c0ea01eeca9d6471169381ae175245d66dfe
Aldi Prahasta <aldidwiki@gmail.com>
2024-07-11 10:35:07 +0700

    implement splash screen

--- app/src/main/java/com/aldiprahasta/tmdb/MainActivity.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/MainActivity.kt
@@ -8,12 +8,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.aldiprahasta.tmdb.ui.home.HomeScreen
 import com.aldiprahasta.tmdb.ui.theme.TMDbTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             TMDbContent()
         }
