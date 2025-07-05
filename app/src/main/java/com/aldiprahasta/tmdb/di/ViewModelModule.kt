745fb15249f6695d7502290c9b0370a9c4b93557
aldidwiki <aldidwiki@gmail.com>
2025-07-05 09:29:00 +0700

    [chore] ViewModelModule : resolve deprecation

--- app/src/main/java/com/aldiprahasta/tmdb/di/ViewModelModule.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/di/ViewModelModule.kt
@@ -8,7 +8,7 @@ import com.aldiprahasta.tmdb.ui.movie.MovieViewModel
 import com.aldiprahasta.tmdb.ui.person.PersonViewModel
 import com.aldiprahasta.tmdb.ui.search.SearchViewModel
 import com.aldiprahasta.tmdb.ui.tv.TvViewModel
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
