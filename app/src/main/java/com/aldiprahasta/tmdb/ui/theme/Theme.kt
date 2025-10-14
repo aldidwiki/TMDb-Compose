1e32aa685a1f2e4c8cd6a4d67e47245e25f70d25
Aldi Prahasta <aldidwiki@gmail.com>
2025-10-14 16:27:28 +0700

    Set status bar color to theme primary color
    Updated TMDbTheme to set the status bar color to the theme's primary color for improved UI consistency. Also removed unused edge-to-edge code from MainActivity.

--- app/src/main/java/com/aldiprahasta/tmdb/ui/theme/Theme.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -56,6 +57,7 @@ fun TMDbTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view)
                     .isAppearanceLightStatusBars = isColorLight(window.statusBarColor)
         }
