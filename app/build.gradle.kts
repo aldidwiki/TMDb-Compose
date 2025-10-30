1c787aac8b436e934f6cb42c88e89dee60b1ef98
Aldi Prahasta <aldidwiki@gmail.com>
2025-10-30 12:50:52 +0700

    Update compileSdk and targetSdk to 36
    Bumped compileSdk and targetSdk versions from 35 to 36 in app/build.gradle.kts to support the latest Android SDK.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ plugins {
 
 android {
     namespace = "com.aldiprahasta.tmdb"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.aldiprahasta.tmdb"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.3"
 
