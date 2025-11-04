d1e9b598c16169b1903fb55ff2ae77952d67998b
Aldi Prahasta <aldidwiki@gmail.com>
2025-11-04 16:29:41 +0700

    Set transparent status and navigation bars

--- app/src/main/java/com/aldiprahasta/tmdb/MainActivity.kt
+++ app/src/main/java/com/aldiprahasta/tmdb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.aldiprahasta.tmdb
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.aldiprahasta.tmdb.ui.home.HomeScreen
 import com.aldiprahasta.tmdb.ui.theme.TMDbTheme
@@ -17,7 +20,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+                statusBarStyle = SystemBarStyle.dark(
+                        Color.Transparent.toArgb(),
+                ),
+                navigationBarStyle = SystemBarStyle.dark(
+                        Color.Transparent.toArgb(),
+                )
+        )
 
         setContent {
             TMDbContent()
