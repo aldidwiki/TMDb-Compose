a4722b87275290f8ed75d885612a481d4958d365
Aldi Prahasta <aldidwiki@gmail.com>
2025-11-06 09:08:42 +0700

    Refactor dependencies with version catalog bundles

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.kotlinParcelize) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.google.service) apply false
+    alias(libs.plugins.google.services) apply false
 }
